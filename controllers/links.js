f4c25bfcc389a10f196ffa0f1f82f2aeb9347f9f
Bennett Lin <bennettslin@gmail.com>
2015-04-03 11:54:21 -0700

    final

---
+++ controllers/links.js
@@ -0,0 +1,50 @@
+var db = require('../models');
+
+var Hashids = require("hashids");
+var salt = "This is Bennett's salt.";
+var hashids = new Hashids(salt);
+
+var express = require('express');
+var router = express.Router();
+
+router.get("/", function(req, res) {
+  db.link.findAll().then(function(links) {
+    var linkArray = links.map(function(link) {
+      return {url: link.url, hash: link.hash, count: link.count};
+    });
+
+  linkArray.sort(function(a, b) {
+    if (a.count < b.count) {
+      return 1;
+    } else if (a.count > b.count) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+    var locals = {links: linkArray};
+    res.render("links/index", locals);
+  })
+});
+
+router.post("/", function(req, res) {
+  db.link.create({url:req.body.url}).then(function(createdLink) {
+    var hash = hashids.encode(createdLink.id);
+    createdLink.hash = hash;
+    createdLink.count = 0;
+    createdLink.save();
+    res.render("links/show", {hash:hash, count:createdLink.count});
+  }).catch(function(error) {
+    console.log(error);
+  });
+});
+
+router.get("/:id", function(req, res) {
+  var link_id = parseInt(hashids.decode(req.params.id));
+  db.link.find(link_id).then(function(link) {
+    res.render("links/show", {hash:req.params.id, count:link.count});
+  });
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,35 @@
+var db = require('./models');
+
+var Hashids = require("hashids");
+var salt = "This is Bennett's salt.";
+var hashids = new Hashids(salt);
+
+var express = require('express');
+var linksCtrl = require('./controllers/links');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/links", linksCtrl);
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/:hash", function(req, res) {
+  var link_id = parseInt(hashids.decode(req.params.hash));
+  db.link.find(link_id).then(function(accessedLink) {
+    accessedLink.count = accessedLink.count + 1;
+    accessedLink.save();
+    res.redirect('http://' + accessedLink.url);
+  }).catch(function(error) {
+    console.log(error);
+  });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server running on port 3000.");
+});
